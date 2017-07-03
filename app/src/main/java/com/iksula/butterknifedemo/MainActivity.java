0cecdb74402c829d7d638aad2ce3f0d141301d3a
Ragini-S <ragini.s@iksula.com>
2017-07-03 12:05:45 +0530

    Initial commit

---
+++ app/src/main/java/com/iksula/butterknifedemo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.iksula.butterknifedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @InjectView(R.id.txtUsername)
+    EditText edtUsername;
+
+    @InjectView(R.id.txtPassword)
+    EditText edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ButterKnife.inject(this);
+
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void login()
+    {
+
+        Toast.makeText(MainActivity.this, "Hi " + edtUsername.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
